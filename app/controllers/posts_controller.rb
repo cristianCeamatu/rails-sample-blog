45bc76d288c555b20cef979beeb63222f2c24d8b
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-08-19 17:55:11 +0300

    Basic valdiation for create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,19 +3,26 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
   
-  
   def show
     @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
   def create
     @post = Post.new(post_params)
 
-    @post.save
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   private
