5762abf3000949b925cb5f2b2d537d640112ff50
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-08-27 17:01:28 +0300

    restore files

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'home/index'
-
   root 'home#index'
-
   resources :posts
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
